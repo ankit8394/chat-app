6857dd7fc49b3f0e05948007fbf9471c6ecccbee
Ankit <ankit0839483@gmail.com>
2025-02-11 23:07:50 +0530

    signup with jwt completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,54 @@
-export const signup = (req,res)=>{
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js"
 
-    const {fullName,email,password} = req.body
+import bcrypt from "bcryptjs"
+
+export const signup = async (req,res)=>{
+
+    const {fullName,email,password} = req.body;
 
     try{
-        //hash password
-        
-    }catch(error)
-    {
+        if(password.length<8){
+            return res.status(400).json({message: "Password must be at least 8 character"});
+        }
+        const user = await User.findOne({email})
+
+        if(user) return res.status(400).json({message: "email already exists"});
+         
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            //generate jwt token here
+            generateToken (newUser._id,res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+
+        }
+        else{
+            res.status(400).json({message:"Invalid user data"});
+        }
 
     }
+
+    catch(error)
+        {
+            console.log("error in signup controller", error.message);   
+            res.status(500).json({message:"Internal server error"});
+            
+        }
 };
 
 export const login = (req,res)=>{
