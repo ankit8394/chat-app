823804f6a74546e1204f90d1699547eb0c263c82
Ankit <ankit0839483@gmail.com>
2025-02-25 00:02:16 +0530

    making signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [formData, setFormData] = useState({
+
+    fullName:"",
+    email:"",
+    password:"",
+
+  });
+
+  const { signup, isSignigUp } = useAuthStore();
+
+  const validateForm = () => {}
+  const validateForm = (e) => {
+    e.preventDefault()
+  }
+
+
+
+
+
   return (
     <div>
       signup
     </div>
   )
-}
+};
 
 export default SignUpPage
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -26,6 +26,9 @@ export const useAuthStore = create((set)=>({
         finally{
             set({isCheckingAuth:false});
         }
-    }
+    },
 
+    signup: async (data) =>{
+        
+    }
 }));
