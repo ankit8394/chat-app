ab9fc4cbcec0d5a7dc9a68889dd1d68c3f6a4204
Ankit <ankit0839483@gmail.com>
2025-02-27 23:51:06 +0530

    signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore';
+import { Eye, EyeOff, Mail, MessageSquare, User } from 'lucide-react';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -15,19 +16,80 @@ const SignUpPage = () => {
   const { signup, isSignigUp } = useAuthStore();
 
   const validateForm = () => {}
-  const validateForm = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
   }
 
 
 
-
-
   return (
-    <div>
-      signup
+    <div className='min-h-screen grid lg:grid-cols-2'>    
+        { /* left side of the form*/  }
+        <div className='flex flex-col justify-center items-center p-6 sm:p-12'>
+            <div className='w-full max-w-md space-y-8'>
+                <div className='text-center mb-8'>
+                    {/* for message logo*/}
+                    <div className='flex flex-col items-center gap-2 group'>
+                        <div className='size-12 rounded-xl bg-primary/10 flex items-center
+                         justify-center group-hover:bg-primary/20 transition-colors'>
+                            <MessageSquare className='size-6 text-primary' />
+                         </div>
+                         <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
+                         <p className='text-base-content/60'>Get started with your free account</p>
+                    </div>
+                </div>
+                <form onSubmit={handleSubmit} className='space-y-6'>
+                    <div className="form-control">
+                        <label className='label'>
+                            <span className='label-text font-medium'>Email</span>
+                        </label>
+                        <div className="relative">
+                            {/* user Icon */}
+                            <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                                <User className='size-5 text-base-content/40' />
+                            </div>
+                            <input 
+                            type="text"
+                            className={`input input-bordered w-full pl-10`}
+                            placeholder='John Doe'
+                            value={formData.fullName}
+                            onChange={(e)=>setFormData({...formData,fullName: e.target.value})}
+                             />
+                        </div>
+                    </div>
+
+                    <div className="form-control">
+                        <label className='label'>
+                            <span className='label-text font-medium'>Full Name</span>
+                        </label>
+                        <div className="relative">
+                            {/* Lock Icon */}
+                            <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                                <Mail className='size-5 text-base-content/40' />
+                            </div>
+                            <input 
+                            type="email"
+                            className={`input input-bordered w-full pl-10`}
+                            placeholder='your@example.com'
+                            value={formData.email}
+                            onChange={(e)=>setFormData({...formData,email: e.target.value})}
+                             />
+                        </div>
+                    </div>
+
+                    <div className="form-control">
+                        <label className='label'>
+                            <span className='label-text font-medium'>Password</span>
+                        </label>
+                        
+                        
+
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
   )
 };
 
-export default SignUpPage
+export default SignUpPage;
