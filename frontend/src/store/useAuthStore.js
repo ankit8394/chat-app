a5a4fff505a5840e024929088ab5b630e9bda905
Ankit <ankit0839483@gmail.com>
2025-04-02 13:02:17 +0530

    final deployment commit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -10,7 +10,7 @@ const BASE_URL =import.meta.env.MODE === "development" ? "http://localhost:5001"
 export const useAuthStore = create((set, get)=>({
     authUser: null,
     isSigningUp:false,
-    isLoggingIng:false,
+    isLoggingIn:false,
     isUpdatingProfile: false,
     isCheckingAuth:true, 
     onlineUsers:[],
@@ -74,7 +74,7 @@ export const useAuthStore = create((set, get)=>({
     },
 
     login: async(data)=>{
-        set({ isLoggingIng: true});
+        set({ isLoggingIn: true});
         try {
             const res = await axiosInstance.post("/auth/login",data);
             set({authUser:res.data});
@@ -90,7 +90,7 @@ export const useAuthStore = create((set, get)=>({
 
         }
         finally{
-            set({isLoggingIng:false});
+            set({isLoggingIn:false});
         }
     },
 
