f142c385344f2085eda12e9781629f8cb481ec47
Ankit <ankit0839483@gmail.com>
2025-04-02 12:08:24 +0530

    ready to deploy to production level

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,13 +6,17 @@ import {connectDB} from "./lib/db.js"
 
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +28,15 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) =>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 server.listen(PORT, ()=> {
     console.log("server is running on port:" + PORT);
     connectDB()
