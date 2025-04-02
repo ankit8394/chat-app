5cdab8f2ffc00bb8646d52ab1df899b1e47118ac
Ankit <ankit0839483@gmail.com>
2025-04-02 12:42:42 +0530

    on deployment

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useAuthStore } from '../store/useAuthStore';
-import AuthImagePattern from '../components/authImagePattern';
+import AuthImagePattern from "../components/AuthImagePattern";
 import { Link } from 'react-router-dom';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare} from 'lucide-react';
 
